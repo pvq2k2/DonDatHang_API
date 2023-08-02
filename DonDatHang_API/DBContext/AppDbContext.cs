476a2338ac7d1e2eb8cb9ab092ffa088fcfc256b
pvq2k2 <vanquyet2392002@gmail.com>
2023-08-02 10:18:40 +0700

    Create AppDbContext - Create Class Settings

---
+++ DonDatHang_API/DBContext/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DonDatHang_API.DBContext
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Settings.MyConnectString());
+        }
+    }
+}
---
+++ DonDatHang_API/DBContext/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace DonDatHang_API.DBContext
+{
+    public class Settings
+    {
+        public static string MyConnectString() {
+            return "Server = DESKTOP-O1GKQUN; Database = DonDatHang_API; Integrated Security = SSPI; Encrypt = True; TrustServerCertificate = True;";
+        }
+    }
+}
